3c74d4ef333b079e577224dda276e38b4f2760f3
Grigoris Tsolakidis <gtsolakidis@deloitte.gr>
2022-04-14 11:22:03 +0300

    Retrieve RabbitMQ connection settings from appsettings.json

--- Rmq.Publisher/Controllers/PublisherController.cs
+++ Rmq.Publisher/Controllers/PublisherController.cs
@@ -11,6 +11,7 @@ using Rmq.Publisher.Models;
 using System.Collections.Concurrent;
 using Newtonsoft.Json;
 using System.Threading;
+using Microsoft.Extensions.Configuration;
 
 namespace Rmq.Publisher.Controllers
 {
@@ -18,22 +19,39 @@ namespace Rmq.Publisher.Controllers
     [ApiController]
     public class PublisherController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public PublisherController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         [HttpPost]
         public IActionResult Post([FromBody] OrderPublish order)
         {
             ConcurrentDictionary<string, OrderPublish> waitingRequests = new ConcurrentDictionary<string, OrderPublish>();
 
-            ConnectionFactory factory = new ConnectionFactory();
-            // "guest"/"guest" by default, limited to localhost connections
-            factory.HostName = "localhost";
-            factory.VirtualHost = "/";
-            factory.Port = 5672;
-            factory.UserName = "guest";
-            factory.Password = "guest";
+            //ConnectionFactory factory = new ConnectionFactory();
+            //// "guest"/"guest" by default, limited to localhost connections
+            //factory.HostName = "localhost";
+            //factory.VirtualHost = "/";
+            //factory.Port = 5672;
+            //factory.UserName = "guest";
+            //factory.Password = "guest";
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                HostName = _configuration["RabbitMqConnection:HostName"].ToString(),
+                VirtualHost = _configuration["RabbitMqConnection:VirtualHost"].ToString(),
+                Port = Convert.ToInt32(_configuration["RabbitMqConnection:Port"]),
+                UserName = _configuration["RabbitMqConnection:Username"].ToString(),
+                Password = _configuration["RabbitMqConnection:Password"].ToString()
+            };
 
             IConnection conn = factory.CreateConnection();
             IModel channel = conn.CreateModel();
+
+            string publishQueue = "publisherQueue";
+            string consumeQueue = "consumerQueue";
 
             OrderConsume responseData = new OrderConsume();
 
@@ -47,9 +65,9 @@ namespace Rmq.Publisher.Controllers
                 responseData = response;
             };
 
-            channel.BasicConsume("consumerQueue", true, consumer);
+            channel.BasicConsume(consumeQueue, true, consumer);
 
-            sendRequest(waitingRequests, channel, new OrderPublish(order.OrderID, order.OrderStatus));
+            sendRequest(waitingRequests, channel, new OrderPublish(order.OrderID, order.OrderStatus), publishQueue);
 
             channel.Close();
             conn.Close();
@@ -57,7 +75,7 @@ namespace Rmq.Publisher.Controllers
             return StatusCode(StatusCodes.Status201Created);
         }
 
-        private static void sendRequest(ConcurrentDictionary<string, OrderPublish> waitingRequest, IModel channel, OrderPublish request)
+        private static void sendRequest(ConcurrentDictionary<string, OrderPublish> waitingRequest, IModel channel, OrderPublish request, string queueName)
         {
             string requestId = Guid.NewGuid().ToString();
             string requestData = JsonConvert.SerializeObject(request);
@@ -70,7 +88,7 @@ namespace Rmq.Publisher.Controllers
 
             channel.BasicPublish(
                 "",
-                "publisherQueue",
+                queueName,
                 basicProperties,
                 Encoding.UTF8.GetBytes(requestData));
         }
