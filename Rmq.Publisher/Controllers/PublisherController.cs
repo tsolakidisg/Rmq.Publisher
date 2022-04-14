de9f51fb65b410dc86485ca6de61d3f8fabae4a6
Grigoris Tsolakidis <gtsolakidis@deloitte.gr>
2022-04-14 14:28:20 +0300

    Added Get Method to retrieve from the Queue

--- Rmq.Publisher/Controllers/PublisherController.cs
+++ Rmq.Publisher/Controllers/PublisherController.cs
@@ -26,6 +26,63 @@ namespace Rmq.Publisher.Controllers
             _configuration = configuration;
         }
 
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // Create a connection factory, using the RabbitMQ configuration settings from appsettings.json
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                HostName = _configuration["RabbitMqConnection:HostName"].ToString(),
+                VirtualHost = _configuration["RabbitMqConnection:VirtualHost"].ToString(),
+                Port = Convert.ToInt32(_configuration["RabbitMqConnection:Port"]),
+                UserName = _configuration["RabbitMqConnection:Username"].ToString(),
+                Password = _configuration["RabbitMqConnection:Password"].ToString()
+            };
+
+            IConnection conn = factory.CreateConnection();
+            IModel channel = conn.CreateModel();
+
+            // Queue name to retrieve from
+            string consumeQueue = "consumerQueue";
+
+            // Initialize the message string and the number of retries
+            string message = string.Empty;
+            int retries;
+            // Max 3 retries to read from the queue
+            for (retries = 1; retries <= 3; retries++)
+            {
+                try
+                {
+                    // Get the message from the queue without acknowledging it yet
+                    BasicGetResult result = channel.BasicGet(consumeQueue, false);
+                    if (result != null)
+                    {
+                        // Decode the received message, deserialize it into a JSON object and acknowledge its processing
+                        message = Encoding.UTF8.GetString(result.Body.Span);
+                        ResponseModel response = JsonConvert.DeserializeObject<ResponseModel>(message);
+                        channel.BasicAck(result.DeliveryTag, false);
+                        retries = 10; // Set retries to 10 (>3) to exit the loop and use it later as a flag to depict the message receival
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+
+            }
+
+            if (retries >= 10)
+            {
+                // The message was retrieved successfully
+                return Ok(message);
+            }
+            else
+            {
+                // The message was not found
+                return NotFound("Order not found (Max retries: 3 reached).");
+            }
+        }
+
         [HttpPost]
         public IActionResult Post([FromBody] RequestModel order)
         {
---
+++ Rmq.Publisher/Models/ResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rmq.Publisher.Models
+{
+    public class ResponseModel
+    {
+        public string Progress { get; set; }
+    }
+}
